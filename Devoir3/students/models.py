8ccc96d0b01d04e74ab60dc8740eb1898d080f45
Maria Bourouisa <maria.bourouisa@polymtl.ca>
2025-11-28 20:28:28 -0500

    Q3 done 😎

--- Devoir3/students/models.py
+++ Devoir3/students/models.py
@@ -162,10 +162,25 @@ class DigitClassificationModel(object):
     methods here. We recommend that you implement the RegressionModel before
     working on this part of the project.)
     """
+    DIMENSION_COUCHE_CACHEE_1 = 128
+    DIMENSION_COUCHE_CACHEE_2 = 64
+    DIMENSION_COUCHE_SORTIE = 10
+    BATCH_SIZE = 100
+    LOSS_THRESHOLD = 0.01
+    LEARNING_RATE = 0.1
 
     def __init__(self) -> None:
-        # Initialize your model parameters here
-        "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        # Couche cachée 1
+        self.w1 = nn.Parameter(784, self.DIMENSION_COUCHE_CACHEE_1)
+        self.b1 = nn.Parameter(1, self.DIMENSION_COUCHE_CACHEE_1)
+
+        # Couche cachée 2
+        self.w2 = nn.Parameter(self.DIMENSION_COUCHE_CACHEE_1, self.DIMENSION_COUCHE_CACHEE_2)
+        self.b2 = nn.Parameter(1, self.DIMENSION_COUCHE_CACHEE_2)
+        
+        # Couche de sortie
+        self.w3 = nn.Parameter(self.DIMENSION_COUCHE_CACHEE_2, self.DIMENSION_COUCHE_SORTIE)
+        self.b3 = nn.Parameter(1, self.DIMENSION_COUCHE_SORTIE)
 
     def run(self, x: nn.Constant) -> nn.Node:
         """
@@ -181,7 +196,20 @@ class DigitClassificationModel(object):
             A node with shape (batch_size x 10) containing predicted scores
                 (also called logits)
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        # Couche cachée 1
+        l1 = nn.Linear(x, self.w1)
+        p1 = nn.AddBias(l1, self.b1)
+        n1 = nn.ReLU(p1)
+
+        # Couche cachée 2
+        l2 = nn.Linear(n1, self.w2)
+        p2 = nn.AddBias(l2, self.b2)
+        n2 = nn.ReLU(p2)
+
+        # Couche de sortie
+        l3 = nn.Linear(n2, self.w3)
+        p3 = nn.AddBias(l3, self.b3)
+        return p3
 
     def get_loss(self, x: nn.Constant, y: nn.Constant) -> nn.Node:
         """
@@ -196,10 +224,30 @@ class DigitClassificationModel(object):
             y: a node with shape (batch_size x 10)
         Returns: a loss node
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        prediction = self.run(x)
+        return nn.SoftmaxLoss(prediction, y)
 
     def train(self, dataset: DigitClassificationDataset) -> None:
         """
         Trains the model.
         """
-        "*** TODO: COMPLETE HERE FOR QUESTION 3 ***"
+        while True:
+            for x, y in dataset.iterate_once(self.BATCH_SIZE):
+                loss_node = self.get_loss(x, y)
+
+                gradients = nn.gradients(loss_node, [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3])
+
+                # Couche cachée 1
+                self.w1.update(gradients[0], -self.LEARNING_RATE)
+                self.b1.update(gradients[1], -self.LEARNING_RATE)
+
+                # Couche cachée 2
+                self.w2.update(gradients[2], -self.LEARNING_RATE)
+                self.b2.update(gradients[3], -self.LEARNING_RATE)
+
+                # Couche de sortie
+                self.w3.update(gradients[4], -self.LEARNING_RATE)
+                self.b3.update(gradients[5], -self.LEARNING_RATE)
+
+            if dataset.get_validation_accuracy() >= 0.97:
+                break
